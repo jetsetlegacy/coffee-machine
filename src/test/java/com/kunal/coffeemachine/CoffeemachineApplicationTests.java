5849a823db5e0433f8a2542814c99112768fcbd7
Kunal Arneja <kunal.arneja@narvar.com>
2020-08-31 15:13:01 +0530

    Initial Commit

---
+++ src/test/java/com/kunal/coffeemachine/CoffeemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.coffeemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeemachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
